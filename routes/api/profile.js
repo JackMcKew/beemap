44373d214a09bd30eb9ec5d13a0b80093fa36cd5
Jack McKew <jackmckew2@gmail.com>
2020-07-10 17:01:57 +1000

    Get profile by id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -118,4 +118,44 @@ router.post('/', [auth, [
 
 })
 
+
+// @route   GET api/profile
+// @desc    Get all profiles
+// @access  Public
+router.get('/', async(req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user', ['name', 'avatar'])
+        res.json(profiles)
+    } catch (error) {
+        console.log(error.message)
+        return res.status(500).send('Server Error')
+    }
+})
+
+
+// @route   GET api/profile/user/:user_id
+// @desc    Get profile by user_id
+// @access  Public
+router.get('/user/:user_id', async(req, res) => {
+    try {
+        const profile = await Profile.findOne({
+            user: req.params.user_id
+        }).populate('user', ['name', 'avatar'])
+        if (!profile) {
+            return res.status(400).send({
+                msg: 'Profile not found for user'
+            })
+        }
+        res.json(profile)
+    } catch (error) {
+        console.log(error.message)
+        if (error.kind == 'ObjectId') {
+            return res.status(400).send({
+                msg: 'Profile not found for user'
+            })
+        }
+        return res.status(500).send('Server Error')
+    }
+})
+
 module.exports = router
