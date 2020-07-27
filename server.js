998d33811aa9ed96d808296f99b07aced5f39733
Jack McKew <jackmckew2@gmail.com>
2020-07-27 11:01:20 +1000

    Prep for production

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const connectDB = require("./config/db")
 const app = express();
-
+const path = require("path");
 // Connect Database
 
 connectDB();
@@ -14,7 +14,7 @@ app.use(express.json({
     extended: false
 }));
 
-app.get("/", (req, res) => res.send("API Running"));
+// app.get("/", (req, res) => res.send("API Running"));
 
 // Define routes
 
@@ -22,6 +22,16 @@ app.use('/api/users', require('./routes/api/users'))
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/posts', require('./routes/api/posts'))
 app.use('/api/profile', require('./routes/api/profile'))
+
+// Serve static assets in production
+
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 
